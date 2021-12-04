73a22d965d05cb5056a9d6a9865e000c2ca80335
DmitryGytnikov <gytnikovd@mail.ru>
2021-12-04 11:06:43 +0300

    Добавляет верстку для десктопа (день1 + день2 стрима)

---
+++ js/main.js
@@ -0,0 +1,57 @@
+$(function () {
+
+  $('.header-slider').slick({
+    dots: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/prev.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/next.svg" alt=""></button>',
+    vertical: true,
+    responsive: [
+      {
+        breakpoint: 371,
+        settings: {
+          dots: false,
+          arrows: false,
+          autoplay: true,
+          autoSpeed: 2000,
+          infinity: true
+        }
+      }
+    ]
+  });
+
+  $('.product__name').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    focusOnSelect: true,
+    asNavFor: '.product__content',
+    vertical: true,
+    prevArrow: '<button type="button" class="product-prev"><img src="images/product-prev.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="product-next"><img src="images/product-next.svg" alt=""></button>',
+    centerMode: true,
+    responsive: [
+      {
+        breakpoint: 891,
+        settings: {
+          vertical: false,
+          slidesToShow: 3,
+          arrows: false,
+          dots: true
+        },
+        breakpoint: 460,
+        settings: {
+          vertical: false,
+          slidesToShow: 1,
+          arrows: false,
+          dots: true
+        },
+      }]
+
+  });
+  $('.product__content').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.product__name',
+    fade: true,
+    arrows: false
+  });
+});
